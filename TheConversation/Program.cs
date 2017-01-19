e72794570640ae2f3ffe7e7fd5c8c57b751267d4
Laramie Bates <laramiebates@gmail.com>
2017-01-19 15:00:35 -0500

    Initial Commit
    Class Assignment 1

---
+++ TheConversation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheConversation
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//
+			// variables
+			//
+
+			string userName;
+			string userInput1;
+			string userInput2;
+
+			//
+			// start the conversation
+			//
+			Console.WriteLine("Whats your name?");
+			userName = Console.ReadLine();
+			Console.WriteLine();
+			Console.WriteLine("Hello, ", userName);
+			// 
+			// pause for user
+			//
+			Console.WriteLine();
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+		}
+	}
+}
